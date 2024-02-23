8461dd9abe8cb83e8c8a0ba760936685345071a1
IMOLEAYO-MICHAEL <adeyeunimoleayo@gmail.com>
2024-02-23 17:20:42 +0100

    I added the alternate test cases for temperature converters

--- src/test/java/com/converter/temperature/TemperatureConverterTest.java
+++ src/test/java/com/converter/temperature/TemperatureConverterTest.java
@@ -13,15 +13,32 @@ class TemperatureConverterTest {
     }
 
     @Test
+    void convertFalseCelsiusToFahrenheit() {
+        double expectedOutput = 500;
+        assertNotEquals(expectedOutput, TemperatureConverter.convertCelsiusToFahrenheit(10));
+    }
+
+    @Test
     void convertFahrenheitToCelsius() {
         double expectedOutput = 10;
         assertEquals(expectedOutput, TemperatureConverter.convertFahrenheitToCelsius(50));
     }
 
     @Test
+    void convertFalseFahrenheitToCelsius() {
+        double expectedOutput = 100;
+        assertNotEquals(expectedOutput, TemperatureConverter.convertFahrenheitToCelsius(50));
+    }
+    @Test
     void convertCelsiusToKelvin() {
         double expectedOutput = 283.15;
         assertEquals(expectedOutput, TemperatureConverter.convertCelsiusToKelvin(10));
+    }
+
+    @Test
+    void convertFalseCelsiusToKelvin() {
+        double expectedOutput = 200.15;
+        assertNotEquals(expectedOutput, TemperatureConverter.convertCelsiusToKelvin(10));
     }
 
     @Test
@@ -31,14 +48,32 @@ class TemperatureConverterTest {
     }
 
     @Test
+    void convertFalseKelvinToCelsius() {
+        double expectedOutput = 100;
+        assertNotEquals(expectedOutput, TemperatureConverter.convertKelvinToCelsius(283.15));
+    }
+
+    @Test
     void convertFahrenheitToKelvin() {
         double expectedOutput = 283.15;
         assertEquals(expectedOutput, TemperatureConverter.convertFahrenheitToKelvin(50));
     }
 
     @Test
+    void convertFalseFahrenheitToKelvin() {
+        double expectedOutput = 200.15;
+        assertNotEquals(expectedOutput, TemperatureConverter.convertFahrenheitToKelvin(50));
+    }
+
+    @Test
     void convertKelvinToFahrenheit() {
         double expectedOutput = 50;
         assertEquals(expectedOutput, TemperatureConverter.convertKelvinToFahrenheit(283.15));
+    }
+
+    @Test
+    void convertFalseKelvinToFahrenheit() {
+        double expectedOutput = 80;
+        assertNotEquals(expectedOutput, TemperatureConverter.convertKelvinToFahrenheit(283.15));
     }
 }
