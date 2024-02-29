3f0752ee8e26839c66a738b6bebf4d08e3184707
enoch-aik <enoch_aik@icloud.com>
2024-02-29 17:22:14 +0100

    - Added length converter UI

--- src/main/java/com/converter/ui/ConverterUserInterface.java
+++ src/main/java/com/converter/ui/ConverterUserInterface.java
@@ -1,15 +1,18 @@
 package com.converter.ui;
 
 
+import com.converter.constants.ConverterConstants;
+import com.converter.length.LengthConverter;
+
 import javax.swing.*;
 
-enum ConverterCategories {Length, Temperature, Weight}
+enum ConverterCategories {Length, Temperature, Weight, Volume}
 
 public class ConverterUserInterface {
 
     public static void main(String[] args) {
-        String[] converterCategories = new String[]{"Length", "Temperature", "Weight"};
-        Object selectedCategory = selectCategory(converterCategories);
+        // String[] converterCategories = new String[]{"Length", "Temperature", "Weight"};
+        Object selectedCategory = selectCategory();
 
         //If the user clicks cancel, close the program
         if (selectedCategory == null) {
@@ -22,6 +25,64 @@ public class ConverterUserInterface {
 
         switch (category) {
             case Length -> {
+                Object converter0ption = JOptionPane.showInputDialog(null, "Select the unit you want to convert", "Length Converter", JOptionPane.QUESTION_MESSAGE, null, ConverterConstants.lengthConverterCategory, null);
+                if (converter0ption == null) {
+                    System.exit(0);
+                    return;
+                }
+                String selectedConverter = converter0ption.toString();
+                switch (selectedConverter) {
+                    case "Meter to Feet" -> {
+                        String input = JOptionPane.showInputDialog(null, "Enter the value in meters", "Length Converter", JOptionPane.QUESTION_MESSAGE);
+                        if (input == null) {
+                            System.exit(0);
+                            return;
+                        }
+                        double meters = Double.parseDouble(input);
+                        double feet = LengthConverter.convertMetersToFeet(meters);
+                        JOptionPane.showMessageDialog(null, meters + " meters is equal to " + feet + " feet", "Length Converter", JOptionPane.INFORMATION_MESSAGE);
+                    }
+                    case "Feet to Meter" -> {
+                        String input = JOptionPane.showInputDialog(null, "Enter the value in feet", "Length Converter", JOptionPane.QUESTION_MESSAGE);
+                        if (input == null) {
+                            System.exit(0);
+                            return;
+                        }
+                        double feet = Double.parseDouble(input);
+                        double meters = LengthConverter.convertFeetToMeters(feet);
+                        JOptionPane.showMessageDialog(null, feet + " feet is equal to " + meters + " meters", "Length Converter", JOptionPane.INFORMATION_MESSAGE);
+                    }
+                    case "Meter to Inch" -> {
+                        String input = JOptionPane.showInputDialog(null, "Enter the value in meters", "Length Converter", JOptionPane.QUESTION_MESSAGE);
+                        if (input == null) {
+                            System.exit(0);
+                            return;
+                        }
+                        double meters = Double.parseDouble(input);
+                        double inches = LengthConverter.convertMetersToInches(meters);
+                        JOptionPane.showMessageDialog(null, meters + " meters is equal to " + inches + " inches", "Length Converter", JOptionPane.INFORMATION_MESSAGE);
+                    }
+                    case "Inches to Meter" -> {
+                        String input = JOptionPane.showInputDialog(null, "Enter the value in inches", "Length Converter", JOptionPane.QUESTION_MESSAGE);
+                        if (input == null) {
+                            System.exit(0);
+                            return;
+                        }
+                        double inches = Double.parseDouble(input);
+                        double meters = LengthConverter.convertInchesToMeters(inches);
+                        JOptionPane.showMessageDialog(null, inches + " inches is equal to " + meters + " meters", "Length Converter", JOptionPane.INFORMATION_MESSAGE);
+                    }
+                    case "Meter to Centimeter" -> {
+                        String input = JOptionPane.showInputDialog(null, "Enter the value in meters", "Length Converter", JOptionPane.QUESTION_MESSAGE);
+                        if (input == null) {
+                            System.exit(0);
+                            return;
+                        }
+                        double meters = Double.parseDouble(input);
+                        double centimeters = LengthConverter.convertMetersToCentimeters(meters);
+                        JOptionPane.showMessageDialog(null, meters + " meters is equal to " + centimeters + " centimeters", "Length Converter", JOptionPane.INFORMATION_MESSAGE);
+                    }
+                }
 
 
             }
@@ -31,14 +92,17 @@ public class ConverterUserInterface {
             case Temperature -> {
 
             }
+            case Volume -> {
+
+            }
             default -> {
             }
         }
 
     }
 
-    private static String selectCategory(String[] categories) {
-        return JOptionPane.showInputDialog(null, "Hello!\nWhat do you want to convert?\n\n\n", "Unit Converter", JOptionPane.QUESTION_MESSAGE, null, categories, "Length").toString();
+    private static String selectCategory() {
+        return JOptionPane.showInputDialog(null, "Hello!\nWhat do you want to convert?\n\n\n", "Unit Converter", JOptionPane.QUESTION_MESSAGE, null, com.converter.constants.ConverterConstants.converterCategories, "Length").toString();
 
     }
 
