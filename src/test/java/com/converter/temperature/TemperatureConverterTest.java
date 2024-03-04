99bfd39da6f25b93ef93f4cec297213067b98e7f
Gauri Singh <gaurivinsha@gmail.com>
2024-03-04 16:09:59 +0100

    - added testInput variable for all unit tests - added annotations for weight and temperature conversion tests

--- src/test/java/com/converter/temperature/TemperatureConverterTest.java
+++ src/test/java/com/converter/temperature/TemperatureConverterTest.java
@@ -8,72 +8,96 @@ class TemperatureConverterTest {
 
     @Test
     void convertCelsiusToFahrenheit() {
+        // testing for 10 Celsius to Fahrenheit
+        double testInput = 10;
         double expectedOutput = 50;
-        assertEquals(expectedOutput, TemperatureConverter.convertCelsiusToFahrenheit(10));
+        assertEquals(expectedOutput, TemperatureConverter.convertCelsiusToFahrenheit(testInput));
     }
 
     @Test
     void convertFalseCelsiusToFahrenheit() {
+        // testing for 10 Celsius to Fahrenheit
+        double testInput = 10;
         double expectedOutput = 500;
-        assertNotEquals(expectedOutput, TemperatureConverter.convertCelsiusToFahrenheit(10));
+        assertNotEquals(expectedOutput, TemperatureConverter.convertCelsiusToFahrenheit(testInput));
     }
 
     @Test
     void convertFahrenheitToCelsius() {
+        // testing for 50 Fahrenheit to Celsius
+        double testInput = 50;
         double expectedOutput = 10;
-        assertEquals(expectedOutput, TemperatureConverter.convertFahrenheitToCelsius(50));
+        assertEquals(expectedOutput, TemperatureConverter.convertFahrenheitToCelsius(testInput));
     }
 
     @Test
     void convertFalseFahrenheitToCelsius() {
+        // testing for 50 Fahrenheit to Celsius
+        double testInput = 50;
         double expectedOutput = 100;
-        assertNotEquals(expectedOutput, TemperatureConverter.convertFahrenheitToCelsius(50));
+        assertNotEquals(expectedOutput, TemperatureConverter.convertFahrenheitToCelsius(testInput));
     }
     @Test
     void convertCelsiusToKelvin() {
+        // testing for 10 Celsius to Kelvin
+        double testInput = 10;
         double expectedOutput = 283.15;
-        assertEquals(expectedOutput, TemperatureConverter.convertCelsiusToKelvin(10));
+        assertEquals(expectedOutput, TemperatureConverter.convertCelsiusToKelvin(testInput));
     }
 
     @Test
     void convertFalseCelsiusToKelvin() {
+        // testing for 10 Celsius to Kelvin
+        double testInput = 10;
         double expectedOutput = 200.15;
-        assertNotEquals(expectedOutput, TemperatureConverter.convertCelsiusToKelvin(10));
+        assertNotEquals(expectedOutput, TemperatureConverter.convertCelsiusToKelvin(testInput));
     }
 
     @Test
     void convertKelvinToCelsius() {
+        // testing for 283.15 Kelvin to Celsius
+        double testInput = 283.15;
         double expectedOutput = 10;
-        assertEquals(expectedOutput, TemperatureConverter.convertKelvinToCelsius(283.15));
+        assertEquals(expectedOutput, TemperatureConverter.convertKelvinToCelsius(testInput));
     }
 
     @Test
     void convertFalseKelvinToCelsius() {
+        // testing for 283.15 Kelvin to Celsius
+        double testInput = 283.15;
         double expectedOutput = 100;
-        assertNotEquals(expectedOutput, TemperatureConverter.convertKelvinToCelsius(283.15));
+        assertNotEquals(expectedOutput, TemperatureConverter.convertKelvinToCelsius(testInput));
     }
 
     @Test
     void convertFahrenheitToKelvin() {
+        // testing for 50 Fahrenheit to Kelvin
+        double testInput = 50;
         double expectedOutput = 283.15;
-        assertEquals(expectedOutput, TemperatureConverter.convertFahrenheitToKelvin(50));
+        assertEquals(expectedOutput, TemperatureConverter.convertFahrenheitToKelvin(testInput));
     }
 
     @Test
     void convertFalseFahrenheitToKelvin() {
+        // testing for 50 Fahrenheit to Kelvin
+        double testInput = 50;
         double expectedOutput = 200.15;
-        assertNotEquals(expectedOutput, TemperatureConverter.convertFahrenheitToKelvin(50));
+        assertNotEquals(expectedOutput, TemperatureConverter.convertFahrenheitToKelvin(testInput));
     }
 
     @Test
     void convertKelvinToFahrenheit() {
+        // testing for 283.15 Kelvin to Fahrenheit
+        double testInput = 283.15;
         double expectedOutput = 50;
-        assertEquals(expectedOutput, TemperatureConverter.convertKelvinToFahrenheit(283.15));
+        assertEquals(expectedOutput, TemperatureConverter.convertKelvinToFahrenheit(testInput));
     }
 
     @Test
     void convertFalseKelvinToFahrenheit() {
+        // testing for 283.15 Kelvin to Fahrenheit
+        double testInput = 283.15;
         double expectedOutput = 80;
-        assertNotEquals(expectedOutput, TemperatureConverter.convertKelvinToFahrenheit(283.15));
+        assertNotEquals(expectedOutput, TemperatureConverter.convertKelvinToFahrenheit(testInput));
     }
 }
--- src/test/java/com/converter/volume/VolumeConverterTest.java
+++ src/test/java/com/converter/volume/VolumeConverterTest.java
@@ -9,83 +9,95 @@ public class VolumeConverterTest {
     @org.junit.jupiter.api.Test
     void convertLitersToGallon() {
         // testing for conversion of 20 liters to gallon
+        double testInput = 20;
         double expectedOutput = 5.284015852047556;
-        assertEquals(expectedOutput, VolumeConverter.convertLitersToGallon(20));
+        assertEquals(expectedOutput, VolumeConverter.convertLitersToGallon(testInput));
     }
 
     @org.junit.jupiter.api.Test
     void convertFalseLitersToGallon() {
         // testing for false conversion of 20 liters to gallon
+        double testInput = 20;
         double expectedOutput = 15;
-        assertNotEquals(expectedOutput, VolumeConverter.convertLitersToGallon(20));
+        assertNotEquals(expectedOutput, VolumeConverter.convertLitersToGallon(testInput));
     }
 
     @org.junit.jupiter.api.Test
     void convertGallonToLiters() {
         // testing for conversion of 20 gallons to liters
+        double testInput = 20;
         double expectedOutput = 75.7;
-        assertEquals(expectedOutput, VolumeConverter.convertGallonToLiters(20));
+        assertEquals(expectedOutput, VolumeConverter.convertGallonToLiters(testInput));
     }
 
     @org.junit.jupiter.api.Test
     void convertFalseGallonToLiters() {
         // testing for false conversion of 20 gallons to liters
+        double testInput = 20;
         double expectedOutput = 95;
-        assertNotEquals(expectedOutput, VolumeConverter.convertGallonToLiters(20));
+        assertNotEquals(expectedOutput, VolumeConverter.convertGallonToLiters(testInput));
     }
 
     @org.junit.jupiter.api.Test
     void convertLitersToMilliliters() {
         // testing for conversion of 20 liters to milliliters
+        double testInput = 20;
         double expectedOutput = 20000;
-        assertEquals(expectedOutput, VolumeConverter.convertLitersToMilliliters(20));
+        assertEquals(expectedOutput, VolumeConverter.convertLitersToMilliliters(testInput));
     }
 
     @org.junit.jupiter.api.Test
     void convertFalseLitersToMilliliters() {
         // testing for False conversion of 20 liters to milliliters
+        double testInput = 20;
         double expectedOutput = 200;
-        assertNotEquals(expectedOutput, VolumeConverter.convertLitersToMilliliters(20));
+        assertNotEquals(expectedOutput, VolumeConverter.convertLitersToMilliliters(testInput));
     }
 
     @org.junit.jupiter.api.Test
     void convertMillilitersToLiters() {
         // testing for conversion of 20 milliliters to liters
+        double testInput = 20;
         double expectedOutput = 0.02;
-        assertEquals(expectedOutput, VolumeConverter.convertMillilitersToLiters(20));
+        assertEquals(expectedOutput, VolumeConverter.convertMillilitersToLiters(testInput));
     }
 
     @org.junit.jupiter.api.Test
     void convertFalseMillilitersToLiters() {
         // testing for False conversion of 20 milliliters to liters
+        double testInput = 20;
         double expectedOutput = 200;
-        assertNotEquals(expectedOutput, VolumeConverter.convertMillilitersToLiters(20));
+        assertNotEquals(expectedOutput, VolumeConverter.convertMillilitersToLiters(testInput));
     }
 
     @org.junit.jupiter.api.Test
     void convertTablespoonToMilliliters() {
         // testing for conversion of 1 tablespoon to milliliters
+        double testInput = 1;
         double expectedOutput = 14.79;
-        assertEquals(expectedOutput, VolumeConverter.convertTablespoonToMilliliters(1));
+        assertEquals(expectedOutput, VolumeConverter.convertTablespoonToMilliliters(testInput));
     }
 
     @org.junit.jupiter.api.Test
     void convertFalseTablespoonToMilliliters() {
         // testing for False conversion of 1 tablespoon to milliliters
+        double testInput = 1;
         double expectedOutput = 20;
-        assertNotEquals(expectedOutput, VolumeConverter.convertTablespoonToMilliliters(1));
+        assertNotEquals(expectedOutput, VolumeConverter.convertTablespoonToMilliliters(testInput));
     }
     @org.junit.jupiter.api.Test
     void convertMillilitersToTablespoon() {
         // testing for conversion of 50 milliliters to tablespoon
+        double testInput = 50;
         double expectedOutput = 3.0;
-        assertEquals(expectedOutput, VolumeConverter.convertMillilitersToTablespoon(50));
+        assertEquals(expectedOutput, VolumeConverter.convertMillilitersToTablespoon(testInput));
     }
 
     @org.junit.jupiter.api.Test
     void convertFalseMillilitersToTablespoon() {
         // testing for conversion of 50 milliliters to tablespoon
+        double testInput = 10;
         double expectedOutput = 5.0;
-        assertNotEquals(expectedOutput, VolumeConverter.convertMillilitersToTablespoon(10));
+        assertNotEquals(expectedOutput, VolumeConverter.convertMillilitersToTablespoon(testInput));
     }
 }
