ac40fa80a5aadce6d16e4fb98bbe38aa2ef72c67
IMOLEAYO-MICHAEL <adeyeunimoleayo@gmail.com>
2024-03-07 16:44:35 +0100

    - added for weight integration test

--- src/test/java/com/converter/weight/WeightConverterTest.java
+++ src/test/java/com/converter/weight/WeightConverterTest.java
@@ -47,16 +47,6 @@ class WeightConverterTest {
         assertNotEquals(expectedOutput, WeightConverter.convertPoundsToKilograms(testInput));
     }
 
-     @Test
-    void convertKilogramsToGrams() {
-        // test input in kilograms
-        double testInput = 10;
-        // expected output in grams
-        double expectedOutput = 10000;
-        // assertion statement to check if expected output and actual output are same
-        assertEquals(expectedOutput, WeightConverter.convertKilogramsToGrams(testInput));
-    }
-
     @Test
     void convertFalseKilogramsToGrams() {
         // test input in kilograms
@@ -105,16 +95,6 @@ class WeightConverterTest {
         double expectedOutput = 45.92;
         // assertion statement to check if expected output and actual output are not same
         assertNotEquals(expectedOutput, WeightConverter.convertGramsToMilligrams(testInput));
-    }
-
-   @Test
-    void convertGramsToPounds() {
-        // test input in grams
-        double testInput = 10;
-        // expected output in pounds
-        double expectedOutput = 0.022046244201837775;
-        // assertion statement to check if expected output and actual output are same
-        assertEquals(expectedOutput, WeightConverter.convertGramsToPounds(testInput));
     }
 
     @Test
