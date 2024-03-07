4fb335987948f8b23f9c34e3032b64c351804f7e
Enoch <enoch_aik@icloud.com>
2024-03-07 16:03:59 +0100

    - updated weight converter - added mg to g and g to mg to UI

---
+++ src/test/java/com/converter/integration/IntegrationLengthConverterTest.java
@@ -0,0 +1,75 @@
+package com.converter.integration;
+
+import com.converter.length.LengthConverter;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+//This file is used to run integration test for the length converter
+class IntegrationLengthConverterTest {
+
+    //test to convert feet to inches
+    @org.junit.jupiter.api.Test
+    void convertFeetToInch() {
+        //input value in feet
+        double testInput = 10;
+        //expected output value in inches
+        double expectedOutput = 120.00006095999805;
+        //assertion to check if the expected output and the actual output are same
+        assertEquals(expectedOutput, LengthConverter.convertMetersToInches(LengthConverter.convertFeetToMeters(testInput)));
+    }
+
+    //test to convert inch to feet
+    @org.junit.jupiter.api.Test
+    void convertInchToFeet() {
+        //input value in inches
+        double testInput = 120.00006095999805;
+        //expected output value in feet
+        double expectedOutput = 10;
+        //assertion to check if the expected output and the actual output are same
+        assertEquals(expectedOutput, LengthConverter.convertMetersToFeet(LengthConverter.convertInchesToMeters(testInput)));
+    }
+
+    //test to convert feet to centimeters
+    @org.junit.jupiter.api.Test
+    void convertFeetToCentimeter() {
+        //input value in feet
+        double testInput = 10;
+        //expected output value in centimeters
+        double expectedOutput = 304.7999902464003;
+        //assertion to check if the expected output and the actual output are same
+        assertEquals(expectedOutput, LengthConverter.convertMetersToCentimeters(LengthConverter.convertFeetToMeters(testInput)));
+    }
+
+    //test to convert centimeters to feet
+    @org.junit.jupiter.api.Test
+    void convertCentimeterToFeet() {
+        //input value in centimeters
+        double testInput = 304.7999902464003;
+        //expected output value in feet
+        double expectedOutput = 10;
+        //assertion to check if the expected output and the actual output are same
+        assertEquals(expectedOutput, LengthConverter.convertMetersToFeet(LengthConverter.convertCentimetersToMeters(testInput)));
+    }
+
+    //test to convert centimeters to inches
+    @org.junit.jupiter.api.Test
+    void convertCentimeterToInch() {
+        //input value in centimeters
+        double testInput = 25;
+        //expected output value in inches
+        double expectedOutput = 9.842525;
+        //assertion to check if the expected output and the actual output are same
+        assertEquals(expectedOutput, LengthConverter.convertMetersToInches(LengthConverter.convertCentimetersToMeters(testInput)));
+    }
+
+    //test to convert inches to centimeters
+    @org.junit.jupiter.api.Test
+    void convertInchesToCentimeters() {
+        //input value in inches
+        double testInput = 9.842525;
+        //expected output value in centimeters
+        double expectedOutput = 25;
+        //assertion to check if the expected output and the actual output are same
+        assertEquals(expectedOutput, LengthConverter.convertMetersToCentimeters(LengthConverter.convertInchesToMeters(testInput)));
+    }
+}
--- src/test/java/com/converter/integration/IntegrationWeightConverterTest.java
+++ src/test/java/com/converter/integration/IntegrationWeightConverterTest.java
@@ -4,27 +4,53 @@ package com.converter.integration;
 import com.converter.weight.WeightConverter;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class IntegrationWeightConverterTest {
 
 
+    //Integration test to convert kilograms to milligrams
     @org.junit.jupiter.api.Test
-    void convertGramsToPounds() {
-        // test input in grams
-        double testInput = 10;
-        // expected output in pounds
-        double expectedOutput = 0.022046244201837775;
-        // assertion statement to check if expected output and actual output are same
-        assertEquals(expectedOutput, WeightConverter.convertGramsToPounds(testInput));
+    void convertKilogramsToMilligrams() {
+        //input value in kilograms
+        double testInput = 15;
+        //expected output value in milligrams
+        double expectedOutput = 15000000;
+        //assertion to check if the expected output and the actual output are same
+        assertEquals(expectedOutput, WeightConverter.convertGramsToMilligrams(WeightConverter.convertKilogramsToGrams(testInput)));
     }
 
+
+    //Integration test to convert milligrams to kilograms
     @org.junit.jupiter.api.Test
-    void convertKilogramsToGrams() {
-        // test input in kilograms
+    void convertMilligramsToKilograms() {
+        //input value in milligrams
+        double testInput = 1000000;
+        //expected output value in kilograms
+        double expectedOutput = 1;
+        //assertion to check if the expected output and the actual output are same
+        assertEquals(expectedOutput, WeightConverter.convertGramsToKilograms(WeightConverter.convertMilligramsToGrams(testInput)));
+    }
+
+    //Integration test to convert grams to pounds
+    @org.junit.jupiter.api.Test
+    void convertGramsToPounds() {
+        //input value in grams
+        double testInput = 100;
+        //expected output value in pounds
+        double expectedOutput = 0.220462;
+        //assertion to check if the expected output and the actual output are same
+        assertEquals(expectedOutput, WeightConverter.convertKilogramsToPounds(WeightConverter.convertGramsToKilograms(testInput)));
+    }
+
+    //Integration test to convert pounds to grams
+    @org.junit.jupiter.api.Test
+    void convertPoundsToGrams() {
+        //input value in pounds
         double testInput = 10;
-        // expected output in grams
-        double expectedOutput = 10000;
-        // assertion statement to check if expected output and actual output are same
-        assertEquals(expectedOutput, WeightConverter.convertKilogramsToGrams(testInput));
+        //expected output value in grams
+        double expectedOutput = 4535.929094356397;
+        //assertion to check if the expected output and the actual output are same
+        assertEquals(expectedOutput, WeightConverter.convertKilogramsToGrams(WeightConverter.convertPoundsToKilograms(testInput)));
     }
 
 
