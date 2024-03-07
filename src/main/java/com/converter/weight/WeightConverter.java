844255d4b0f6e3aef2673ab360998d4d3cc87f2f
IMOLEAYO-MICHAEL <adeyeunimoleayo@gmail.com>
2024-03-07 17:11:14 +0100

    - added weight converter for testing

--- src/main/java/com/converter/weight/WeightConverter.java
+++ src/main/java/com/converter/weight/WeightConverter.java
@@ -4,6 +4,33 @@ import com.converter.constants.ConverterConstants;
 
 public class WeightConverter {
     //convert kilograms to pounds
+    public static double convertKilogramsToPounds(double kilograms) {
+        return kilograms * ConverterConstants.KILOGRAM_POUND_UNIT;
+    }
 
+    //convert pounds to kilograms
+    public static double convertPoundsToKilograms(double pounds) {
+        return pounds / ConverterConstants.KILOGRAM_POUND_UNIT;
+    }
+
+    //convert kilograms to grams
+    public static double convertKilogramsToGrams(double kilograms) {
+        return kilograms * ConverterConstants.KILOGRAM_GRAM_UNIT;
+    }
+
+    //convert grams to kilograms
+    public static double convertGramsToKilograms(double grams) {
+        return grams / ConverterConstants.KILOGRAM_GRAM_UNIT;
+    }
+
+    //convert pounds to grams
+    public static double convertGramsToMilligrams(double pounds) {
+        return pounds * ConverterConstants.GRAM_MILLIGRAM_UNIT;
+    }
+
+    //convert grams to pounds
+    public static double convertMilligramsToGrams(double grams) {
+        return grams / ConverterConstants.GRAM_MILLIGRAM_UNIT;
+    }
 }
 
